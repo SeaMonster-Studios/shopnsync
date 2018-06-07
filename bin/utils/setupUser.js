927b379379bc4106c818fd14cae6e3d1d83d8487
Logan Call <logancall.dev@gmail.com>
2018-06-06 22:25:03 -0600

    Finish init

--- bin/utils/setupUser.js
+++ bin/utils/setupUser.js
@@ -1,27 +1,18 @@
-const fs = require('fs')
-const { promisify } = require('util')
-
-const readdir = promisify(fs.readdir)
+const fs = require('fs-extra')
 
 async function setupUser() {
   const snsDir = `${__dirname}/../user-files`
   const userDir = process.cwd()
-  const userFiles = await readdir(userDir)
-  const snsUserFiles = await readdir(snsDir)
-
-  console.log(snsUserFiles)
+  const userFiles = await fs.readdir(userDir)
+  const snsUserFiles = await fs.readdir(snsDir)
 
   for (let snsFile of snsUserFiles) {
     const found = userFiles.some(userFile => userFile === snsFile)
 
     if (!found) {
-      fs.createReadStream(`${snsDir}/${snsFile}`).pipe(
-        fs.createWriteStream(`${userDir}/${snsFile}`),
-      )
+      await fs.copy(`${snsDir}/${snsFile}`, `${userDir}/${snsFile}`)
     }
   }
-
-  // const srcDir = await readdir(`${process.cwd()}/src`)
   return
 }
 
