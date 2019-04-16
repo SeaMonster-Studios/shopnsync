83e57ee775357861221536fc2ebd2c2a94e44092
Logan Call <logancall.dev@gmail.com>
2019-04-16 14:09:30 -0600

    version update

--- bin/index.js
+++ bin/index.js
@@ -1,75 +1,75 @@
 #!/usr/bin/env node
-const program = require('commander')
-const colors = require('colors')
+const program = require("commander");
+const colors = require("colors");
 //
-const build = require('./options/build')
-const init = require('./options/init')
-const packageOption = require('./options/package')
-const pullForce = require('./options/pull:force')
-const pull = require('./options/pull')
-const push = require('./options/push')
-const pushForce = require('./options/push:force')
-const start = require('./options/start')
+const build = require("./options/build");
+const init = require("./options/init");
+const packageOption = require("./options/package");
+const pullForce = require("./options/pull:force");
+const pull = require("./options/pull");
+const push = require("./options/push");
+const pushForce = require("./options/push:force");
+const start = require("./options/start");
 
 async function initSns() {
   program
-    .version('1.0.0-alpha', '-v, --version')
+    .version("1.0.0-beta.7", "-v, --version")
     .option(
-      'init',
-      'Copy extendable config files: webpack, package.json, config.yml, etc.',
+      "init",
+      "Copy extendable config files: webpack, package.json, config.yml, etc."
     )
-    .option('start', 'Start shopnsync development server.')
+    .option("start", "Start shopnsync development server.")
     .option(
-      'build',
-      'Bundles your assets and places them in the `assets` directory of your theme. It also uploads all file changes to Shopify.',
-    )
-    .option(
-      'pull',
-      "An alias for themekit's `theme download`. It will download the entire theme from shopify to your local machine.",
+      "build",
+      "Bundles your assets and places them in the `assets` directory of your theme. It also uploads all file changes to Shopify."
     )
     .option(
-      'pull:force',
-      "An alias for themekit's `theme download --force`. The same as `sns pull` except it will also disable version checking and force all changes.",
+      "pull",
+      "An alias for themekit's `theme download`. It will download the entire theme from shopify to your local machine."
     )
     .option(
-      'push',
-      "An alias for themekit's `theme replace`. This will completely replace what is on Shopify with what is in your current project directory. This means that any files that are on Shopify but are not on your local disk will be removed from Shopify. Any files that are both on your local disk and Shopify will be updated. Lastly any files that are only on your local disk will be upload to Shopify.",
+      "pull:force",
+      "An alias for themekit's `theme download --force`. The same as `sns pull` except it will also disable version checking and force all changes."
     )
     .option(
-      'push:force',
-      "An alias for themekit's `theme replace --force`. The same as `sns push` except it will also disable version checking and force all changes.",
+      "push",
+      "An alias for themekit's `theme replace`. This will completely replace what is on Shopify with what is in your current project directory. This means that any files that are on Shopify but are not on your local disk will be removed from Shopify. Any files that are both on your local disk and Shopify will be updated. Lastly any files that are only on your local disk will be upload to Shopify."
     )
-    .option('package', 'Zip theme for uploading to Shopify store.')
-    .option('zip', 'alias for pacakge')
-    .parse(process.argv)
+    .option(
+      "push:force",
+      "An alias for themekit's `theme replace --force`. The same as `sns push` except it will also disable version checking and force all changes."
+    )
+    .option("package", "Zip theme for uploading to Shopify store.")
+    .option("zip", "alias for pacakge")
+    .parse(process.argv);
 
   if (program.init) {
-    init()
+    init();
   }
   if (program.start) {
-    start()
+    start();
   }
   if (program.build) {
-    build()
+    build();
   }
   if (program.pull) {
-    pull()
+    pull();
   }
-  if (program['pull:force']) {
-    pullForce()
+  if (program["pull:force"]) {
+    pullForce();
   }
 
   if (program.push) {
-    push()
+    push();
   }
-  if (program['push:force']) {
-    pushForce()
+  if (program["push:force"]) {
+    pushForce();
   }
   if (program.package || program.zip) {
-    packageOption()
+    packageOption();
   }
 }
 
 initSns().catch(error => {
-  console.error(`[${colors.blue('Shopnsync')}] ${colors.red(error)}`)
-})
+  console.error(`[${colors.blue("Shopnsync")}] ${colors.red(error)}`);
+});
